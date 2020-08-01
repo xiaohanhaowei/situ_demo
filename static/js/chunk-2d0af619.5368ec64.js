90868ac280919ca45a614e85037294ba50abd2a0
kai.cao <kai.cao@lynxi.com>
2020-08-01 11:23:41 +0800

    Add static.

---
+++ static/js/chunk-2d0af619.5368ec64.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0af619"],{"0da0":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},r=[],u={},a=u,i=t("2877"),o=Object(i["a"])(a,c,r,!1,null,"56ef01c7",null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d0af619.5368ec64.js.map
---
+++ static/js/chunk-2d221c57.797b9dda.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d221c57"],{cc89:function(c,e,n){"use strict";n.r(e);var t=function(){var c=this,e=c.$createElement,n=c._self._c||e;return n("div",[c._v("404")])},u=[],a={},r=a,s=n("2877"),i=Object(s["a"])(r,t,u,!1,null,"c7eaf620",null);e["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d221c57.797b9dda.js.map
